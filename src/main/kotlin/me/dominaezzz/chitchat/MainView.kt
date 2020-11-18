e77e9171466f14e7f4c3d23d310a0651d3e4f58f
Dominic Fischer <dominicfischer7@gmail.com>
2020-11-18 19:51:01 +0000

    Load room events from database

--- src/main/kotlin/me/dominaezzz/chitchat/MainView.kt
+++ src/main/kotlin/me/dominaezzz/chitchat/MainView.kt
@@ -1,8 +1,10 @@
 package me.dominaezzz.chitchat
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumnFor
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.ListItem
 import androidx.compose.material.MaterialTheme
@@ -13,23 +15,23 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageAsset
 import androidx.compose.ui.graphics.asImageAsset
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import io.github.matrixkt.MatrixClient
 import io.github.matrixkt.models.Presence
 import io.github.matrixkt.models.events.UnsignedData
+import io.github.matrixkt.models.events.contents.room.MemberContent
 import io.github.matrixkt.models.sync.RoomSummary
 import io.github.matrixkt.models.sync.SyncResponse
 import io.github.matrixkt.utils.MatrixJson
 import io.ktor.client.engine.apache.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.NonCancellable
-import kotlinx.coroutines.isActive
+import kotlinx.coroutines.*
 import kotlinx.coroutines.sync.Semaphore
 import kotlinx.coroutines.sync.withPermit
-import kotlinx.coroutines.withContext
 import kotlinx.serialization.json.JsonElement
 import me.dominaezzz.chitchat.db.*
 import org.jetbrains.skija.Image
@@ -98,40 +100,78 @@ fun MainView() {
 			}
 		}
 	}
+	var selectedRoom by remember { mutableStateOf<String?>(null) }
 
-	Column(Modifier.fillMaxSize()) {
-		Text(
-			"Rooms",
-			Modifier.padding(10.dp).align(Alignment.CenterHorizontally),
-			style = MaterialTheme.typography.h5
-		)
+	var timelineEvents by remember { mutableStateOf<List<TimelineItem>>(emptyList()) }
+	var relevantMembers by remember { mutableStateOf<Map<String, MemberContent>>(emptyMap()) }
 
-		Spacer(Modifier.height(5.dp))
+	LaunchedEffect(selectedRoom) {
+		val roomId = selectedRoom ?: return@LaunchedEffect
+		val events: List<TimelineItem>
+		val members: List<Pair<String, MemberContent>>
+		withContext(Dispatchers.IO) {
+			usingConnection { conn ->
+				events = conn.getEventsBetween(roomId, 1, Int.MAX_VALUE)
+				members = conn.getRelevantMembersBetween(roomId, 1, Int.MAX_VALUE)
+			}
+		}
+		timelineEvents = events
+		relevantMembers = members.toMap()
+	}
 
-		LazyColumnFor(rooms ?: emptyList()) { room ->
-			ListItem(
-				text = { Text(room.displayName) },
-				secondaryText = { Text("${room.memberCount} members") },
-				singleLineSecondaryText = true,
-				icon = {
-					val image by produceState<ImageAsset?>(null, room) {
-						value = null
-						if (room.avatarUrl != null) {
-							val url = URI(room.avatarUrl)
-							val data = contentRepo.getContent(url)
-							val image = withContext(Dispatchers.Default) {
-								Image.makeFromEncoded(data).asImageAsset()
+	Row(Modifier.fillMaxSize()) {
+		Column(Modifier.fillMaxWidth(0.3f)) {
+			Text(
+				"Rooms",
+				Modifier.padding(10.dp).align(Alignment.CenterHorizontally),
+				style = MaterialTheme.typography.h5
+			)
+
+			Spacer(Modifier.height(5.dp))
+
+			LazyColumnFor(rooms ?: emptyList()) { room ->
+				ListItem(
+					modifier = Modifier.selectable(
+						selected = selectedRoom == room.id,
+						onClick = { selectedRoom = room.id }
+					),
+					text = { Text(room.displayName, maxLines = 1, overflow = TextOverflow.Ellipsis) },
+					secondaryText = { Text("${room.memberCount} members") },
+					singleLineSecondaryText = true,
+					icon = {
+						val image by produceState<ImageAsset?>(null, room) {
+							value = null
+							if (room.avatarUrl != null) {
+								val url = URI(room.avatarUrl)
+								val data = contentRepo.getContent(url)
+								val image = withContext(Dispatchers.Default) {
+									Image.makeFromEncoded(data).asImageAsset()
+								}
+								value = image
 							}
-							value = image
+						}
+
+						if (image != null) {
+							Image(image!!, Modifier.size(40.dp).clip(CircleShape), contentScale = ContentScale.Crop)
+						} else {
+							Image(Icons.Filled.Contacts, Modifier.size(40.dp))
 						}
 					}
+				)
+			}
+		}
 
-					if (image != null) {
-						Image(image!!, Modifier.size(40.dp).clip(CircleShape), contentScale = ContentScale.Crop)
-					} else {
-						Image(Icons.Filled.Contacts, Modifier.size(40.dp))
-					}
-				}
+		Box(
+			Modifier.fillMaxHeight()
+				.preferredWidth(1.dp)
+				.background(color = Color.Black.copy(alpha = 0.27f))
+		)
+
+		// Timeline
+		LazyColumnFor(timelineEvents, Modifier.fillMaxSize()) { item ->
+			ListItem(
+				text = { Text(item.event.type) },
+				secondaryText = { Text(item.event.content.toString()) }
 			)
 		}
 	}
--- src/main/kotlin/me/dominaezzz/chitchat/db/Queries.kt
+++ src/main/kotlin/me/dominaezzz/chitchat/db/Queries.kt
@@ -1,5 +1,15 @@
 package me.dominaezzz.chitchat.db
 
+import io.github.matrixkt.models.events.MatrixEvent
+import io.github.matrixkt.models.events.contents.room.MemberContent
+import kotlinx.serialization.builtins.ListSerializer
+import kotlinx.serialization.builtins.MapSerializer
+import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.json.JsonArray
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
+import java.sql.Connection
+
 // language=sql
 const val ROOM_INFO_SQL = """
 WITH
@@ -52,3 +62,215 @@ DELETE FROM room_metadata;
 DELETE FROM account_data;
 END;
 """
+
+class TimelineItem(
+	val event: MatrixEvent,
+	val edits: List<JsonObject>,
+	val reactions: Map<String, Int>
+)
+
+fun Connection.getTimelineIdAndOrder(roomId: String, eventId: String): Pair<Int, Int> {
+	return prepareStatement("SELECT timelineId, timelineOrder FROM room_events WHERE roomId = ? AND eventId = ?;").use { stmt ->
+		stmt.setString(1, roomId)
+		stmt.setString(2, eventId)
+		stmt.executeQuery().use { rs ->
+			if (!rs.next()) throw NoSuchElementException("Could not find event with roomId='$roomId' and eventId='$eventId'.")
+			val timelineId = rs.getInt(1)
+			val timelineOrder = rs.getInt(2) // Missing a NULL check here but we can think about that later.
+			timelineId to timelineOrder
+		}
+	}
+}
+
+private val supportedEventTypes = listOf(
+	"m.room.name",
+	"m.room.topic",
+	"m.room.member",
+	"m.room.message",
+	"m.room.avatar",
+	"m.room.canonical_alias",
+
+	"m.room.encryption",
+	"m.room.encrypted",
+	"m.room.create",
+	"m.room.guest_access",
+	"m.room.history_visibility",
+	"m.room.join_rules"
+
+	// "m.room.third_party_invite",
+	// "m.room.pinned_events",
+	// "m.room.power_levels",
+	// "m.room.server_acl",
+	// "m.room.tombstone"
+)
+
+@OptIn(ExperimentalStdlibApi::class)
+fun Connection.getEventsBetween(roomId: String, from: Int, to: Int, eventTypes: List<String> = supportedEventTypes): List<TimelineItem> {
+	val sql = """
+		WITH
+			 event_reactions(roomId, eventId, reaction_obj) AS (
+				 SELECT roomId, eventId, JSON_GROUP_OBJECT(key, key_count)
+				 FROM (
+					 SELECT roomId, eventId, key, COUNT() AS key_count
+					 FROM (
+						 SELECT roomId, JSON_EXTRACT(content, '${'$'}."m.relates_to".event_id') AS eventId, JSON_EXTRACT(content, '${'$'}."m.relates_to".key') AS key
+						 FROM room_events
+						 WHERE type = 'm.reaction' AND JSON_EXTRACT(content, '${'$'}."m.relates_to".rel_type') = 'm.annotation'
+					 )
+					 GROUP BY roomId, eventId, key
+				 )
+				 GROUP BY roomId, eventId
+			 ),
+			 msg_updates(roomId, sender, eventId, newContents) AS (
+				 SELECT roomId, sender, eventId, JSON_GROUP_ARRAY(newContent)
+				 FROM (
+					 SELECT roomId, sender, JSON_EXTRACT(content, '${'$'}."m.relates_to".event_id') AS eventId, JSON_EXTRACT(content, '${'$'}."m.new_content"') AS newContent
+					 FROM room_events
+					 WHERE type = 'm.room.message' AND JSON_EXTRACT(content, '${'$'}."m.relates_to".rel_type') = 'm.replace'
+				 )
+				 GROUP BY roomId, sender, eventId
+			 ),
+			 event_types(type) AS (SELECT value FROM JSON_EACH(?)),
+			 msg_events AS (
+				 SELECT roomId, eventId, sender, timelineOrder, JSON_OBJECT(
+						 'type', type,
+						 'content', JSON(content),
+						 'event_id', eventId,
+						 'sender', sender,
+						 'origin_server_ts', timestamp,
+						 'unsigned', JSON(unsigned),
+						 'room_id', roomId,
+						 'state_key', stateKey,
+						 'prev_content', JSON(prevContent)
+					 ) AS json
+				 FROM room_events
+				 JOIN event_types USING (type)
+				 WHERE timelineId = 0 AND timelineOrder IS NOT NULL AND
+					 (type != 'm.room.message' OR JSON_EXTRACT(content, '${'$'}."m.relates_to".rel_type') IS NOT 'm.replace')
+			 )
+		SELECT json, reaction_obj, newContents, timelineOrder
+		FROM msg_events
+		LEFT JOIN event_reactions USING (roomId, eventId)
+		LEFT JOIN msg_updates USING (roomId, sender, eventId)
+		WHERE roomId = ? AND timelineOrder BETWEEN ? AND ?
+		ORDER BY timelineOrder;
+	"""
+
+	return prepareStatement(sql).use { stmt ->
+		stmt.setString(1, JsonArray(eventTypes.map { JsonPrimitive(it) }).toString())
+		stmt.setString(2, roomId)
+		stmt.setInt(3, from)
+		stmt.setInt(4, to)
+		stmt.executeQuery().use { rs ->
+			buildList {
+				while (rs.next()) {
+					val event = rs.getSerializable(1, MatrixEvent.serializer())!!
+					val reactions = rs.getSerializable(2, MapSerializer(String.serializer(), Int.serializer())).orEmpty()
+					val msgUpdates = rs.getSerializable(3, ListSerializer(JsonObject.serializer())).orEmpty()
+					add(TimelineItem(event, msgUpdates, reactions))
+				}
+			}
+		}
+	}
+}
+
+@OptIn(ExperimentalStdlibApi::class)
+fun Connection.getRelevantMembersBetween(roomId: String, from: Int, to: Int): List<Pair<String, MemberContent>> {
+	val sql = """
+		SELECT stateKey, content
+		FROM room_events
+		WHERE isLatestState AND roomId = ?1 AND type = 'm.room.member' AND stateKey IN (
+			SELECT sender
+			FROM room_events
+			WHERE roomId = ?1 AND timelineId = 0 AND timelineOrder BETWEEN ? AND ?
+		);
+	"""
+
+	return prepareStatement(sql).use { stmt ->
+		stmt.setString(1, roomId)
+		stmt.setInt(2, from)
+		stmt.setInt(3, to)
+		stmt.executeQuery().use { rs ->
+			buildList {
+				while (rs.next()) {
+					val memberUserId = rs.getString(1)!!
+					val memberContent = rs.getSerializable(2, MemberContent.serializer())!!
+					add(memberUserId to memberContent)
+				}
+			}
+		}
+	}
+}
+
+@OptIn(ExperimentalStdlibApi::class)
+fun Connection.getEventChangesBetween(roomId: String, from: Int, to: Int, eventTypes: List<String> = supportedEventTypes): List<Pair<Int, TimelineItem>> {
+	val sql = """
+		WITH
+			 event_reactions(roomId, eventId, reaction_obj, latestOrder) AS (
+				 SELECT roomId, eventId, JSON_GROUP_OBJECT(key, key_count), MAX(latestOrder)
+				 FROM (
+					 SELECT roomId, eventId, key, COUNT() AS key_count, MAX(timelineOrder) AS latestOrder
+					 FROM (
+						  SELECT roomId, JSON_EXTRACT(content, '${'$'}."m.relates_to".event_id') AS eventId, JSON_EXTRACT(content, '${'$'}."m.relates_to".key') AS key, timelineOrder
+						  FROM room_events
+						  WHERE type = 'm.reaction' AND JSON_EXTRACT(content, '${'$'}."m.relates_to".rel_type') = 'm.annotation'
+					 )
+					 GROUP BY roomId, eventId, key
+				 )
+				 GROUP BY roomId, eventId
+			 ),
+			 msg_updates(roomId, sender, eventId, newContents, latestOrder) AS (
+				 SELECT roomId, sender, eventId, JSON_GROUP_ARRAY(newContent), MAX(timelineOrder) AS latestOrder
+				 FROM (
+					  SELECT roomId, sender, JSON_EXTRACT(content, '${'$'}."m.relates_to".event_id') AS eventId, JSON_EXTRACT(content, '${'$'}."m.new_content"') AS newContent, timelineOrder
+					  FROM room_events
+					  WHERE type = 'm.room.message' AND JSON_EXTRACT(content, '${'$'}."m.relates_to".rel_type') = 'm.replace'
+				 )
+				 GROUP BY roomId, sender, eventId
+			 ),
+			 event_types(type) AS (SELECT value FROM JSON_EACH(?1)),
+			 msg_events AS (
+				 SELECT roomId, eventId, sender, timelineOrder, JSON_OBJECT(
+						 'type', type,
+						 'content', JSON(content),
+						 'event_id', eventId,
+						 'sender', sender,
+						 'origin_server_ts', timestamp,
+						 'unsigned', JSON(unsigned),
+						 'room_id', roomId,
+						 'state_key', stateKey,
+						 'prev_content', JSON(prevContent)
+					 ) AS json,
+						ROW_NUMBER() OVER (PARTITION BY roomId ORDER BY timelineOrder) AS rowNum
+				 FROM room_events
+				 JOIN event_types USING (type)
+				 WHERE timelineId = 0 AND timelineOrder IS NOT NULL AND
+					 (type != 'm.room.message' OR JSON_EXTRACT(content, '${'$'}."m.relates_to".rel_type') IS NOT 'm.replace')
+			 )
+		SELECT json, reaction_obj, newContents, rowNum
+		FROM msg_events
+		LEFT JOIN event_reactions USING (roomId, eventId)
+		LEFT JOIN msg_updates USING (roomId, sender, eventId)
+		WHERE roomId = ?2 AND timelineOrder BETWEEN ?3 AND ?4 AND
+			  MAX(IFNULL(msg_updates.latestOrder, timelineOrder), IFNULL(event_reactions.latestOrder, timelineOrder)) > ?4
+		ORDER BY timelineOrder;
+	"""
+
+	return prepareStatement(sql).use { stmt ->
+		stmt.setString(1, JsonArray(eventTypes.map { JsonPrimitive(it) }).toString())
+		stmt.setString(2, roomId)
+		stmt.setInt(3, from)
+		stmt.setInt(4, to)
+		stmt.executeQuery().use { rs ->
+			buildList {
+				while (rs.next()) {
+					val event = rs.getSerializable(1, MatrixEvent.serializer())!!
+					val reactions = rs.getSerializable(2, MapSerializer(String.serializer(), Int.serializer())).orEmpty()
+					val msgUpdates = rs.getSerializable(3, ListSerializer(JsonObject.serializer())).orEmpty()
+					val idx = rs.getInt(4)
+					add(idx to TimelineItem(event, msgUpdates, reactions))
+				}
+			}
+		}
+	}
+}
