0300e35c263a8135ae238334ec80ac21e4341853
Dominic Fischer <dominicfischer7@gmail.com>
2020-11-22 19:26:41 +0000

    Tidy main

--- src/main/kotlin/me/dominaezzz/chitchat/main.kt
+++ src/main/kotlin/me/dominaezzz/chitchat/main.kt
@@ -1,16 +1,7 @@
 package me.dominaezzz.chitchat
 
 import androidx.compose.desktop.Window
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.IntSize
-import androidx.compose.ui.unit.dp
 import me.dominaezzz.chitchat.ui.ChitChatTheme
 
 fun main() {
