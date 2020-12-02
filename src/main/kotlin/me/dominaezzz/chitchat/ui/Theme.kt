6c3db493b98600537b167ff4c4f8d251029835b4
Dominic Fischer <dominicfischer7@gmail.com>
2020-12-02 20:23:23 +0000

    Simplify theming (To be configured later)

--- src/main/kotlin/me/dominaezzz/chitchat/ui/Theme.kt
+++ src/main/kotlin/me/dominaezzz/chitchat/ui/Theme.kt
@@ -4,35 +4,10 @@ import androidx.compose.desktop.DesktopMaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 
-val LightColorPalette = lightColors(
-    primary = Purple700,
-    primaryVariant = Purple800,
-    onPrimary = Color.White,
-    secondary = Color.White,
-    onSecondary = Color.Black,
-    background = Color.White,
-    onBackground = Color.Black,
-    surface = Color.White,
-    onSurface = Color.Black,
-    error = Red800,
-    onError = Color.White
-)
+val LightColorPalette = lightColors()
 
-val DarkColorPalette = darkColors(
-    primary = Purple300,
-    primaryVariant = Purple600,
-    onPrimary = Color.Black,
-    secondary = Color.White,
-    onSecondary = Color.White,
-    background = Color.Black,
-    onBackground = Color.White,
-    surface = Color.Black,
-    onSurface = Color.White,
-    error = Red300,
-    onError = Color.Black
-)
+val DarkColorPalette = darkColors()
 
 @Composable
 fun ChitChatTheme(darkTheme: Boolean = false, content: @Composable () -> Unit) {
