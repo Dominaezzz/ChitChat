588a5c5996f1767fd5afeb9e141d12fc7729074e
Dominic Fischer <dominicfischer7@gmail.com>
2020-12-20 16:30:55 +0000

    Move sync storage logic to another file

---
+++ src/main/kotlin/me/dominaezzz/chitchat/db/SyncStore.kt
@@ -0,0 +1,240 @@
+package me.dominaezzz.chitchat.db
+
+import io.github.matrixkt.MatrixClient
+import io.github.matrixkt.models.Presence
+import io.github.matrixkt.models.events.MatrixEvent
+import io.github.matrixkt.models.events.UnsignedData
+import io.github.matrixkt.models.sync.RoomSummary
+import io.github.matrixkt.models.sync.SyncResponse
+import io.github.matrixkt.utils.MatrixJson
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.sync.Semaphore
+import kotlinx.coroutines.sync.withPermit
+import kotlinx.coroutines.withContext
+import kotlinx.serialization.json.JsonElement
+import java.io.Closeable
+import java.sql.Connection
+import java.sql.Types
+
+
+suspend fun sync(client: MatrixClient, dbSemaphore: Semaphore): SyncResponse {
+	val syncToken = withContext(Dispatchers.IO) {
+		usingConnection { it.getValue("SYNC_TOKEN") }
+	}
+
+	println("Syncing with '$syncToken' as token")
+
+	val sync = client.eventApi.sync(since = syncToken, setPresence = Presence.OFFLINE, timeout = 100000)
+
+	println("Saving sync response")
+
+	dbSemaphore.withPermit {
+		withContext(Dispatchers.IO) {
+			storeSyncResponse(sync, syncToken)
+		}
+	}
+
+	println("Saved sync response")
+
+	return sync
+}
+
+private class InsertUtils(connection: Connection): Closeable {
+	private val roomSummaryStmt = connection.prepareStatement("""
+		INSERT INTO room_metadata(roomId, summary)
+		VALUES (?, ?)
+		ON CONFLICT(roomId) DO UPDATE SET summary=JSON_PATCH(summary, excluded.summary);
+	""")
+	private val eventStmt = connection.prepareStatement("""
+		INSERT INTO room_events(
+			roomId, eventId, type, content, sender, unsigned, stateKey, prevContent, timestamp,
+			timelineOrder
+		)
+		VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+		ON CONFLICT(roomId, eventId) DO UPDATE
+			SET timelineOrder = excluded.timelineOrder
+			WHERE excluded.timelineOrder IS NOT NULL;
+	""")
+	private val paginationTokenStmt = connection.prepareStatement("""
+		INSERT INTO room_pagination_tokens(roomId, eventId, token) VALUES (?, ?, ?);
+	""")
+	private val accountStmt = connection.prepareStatement("""
+		INSERT OR REPLACE INTO account_data(type, roomId, content) VALUES (?, ?, ?);
+	""")
+	private val lastOrderStmt = connection.prepareStatement("""
+		SELECT MAX(timelineOrder) FROM room_events WHERE roomId = ? AND timelineId = 0;
+	""")
+	private val newTimelineStmt = connection.prepareStatement("UPDATE room_events SET timelineId = timelineId + 1 WHERE roomId = ?;")
+	private val deviceEventStmt = connection.prepareStatement("INSERT INTO device_events(type, content, sender) VALUES (?, ?, ?);")
+	private val trackedUsersStmt = connection.prepareStatement("UPDATE tracked_users SET deviceListState = 0 WHERE userId = ?;")
+	private val removeTrackedUserStmt = connection.prepareStatement("DELETE FROM tracked_users WHERE userId = ?;")
+
+	fun updateRoomSummary(roomId: String, summary: RoomSummary): Int {
+		roomSummaryStmt.setString(1, roomId)
+		roomSummaryStmt.setString(2, MatrixJson.encodeToString(RoomSummary.serializer(), summary))
+		return roomSummaryStmt.executeUpdate()
+	}
+
+	fun insertRoomEvent(roomId: String, event: MatrixEvent, timelineOrder: Long?): Int {
+		eventStmt.setString(1, roomId)
+		eventStmt.setString(2, event.eventId)
+		eventStmt.setString(3, event.type)
+		eventStmt.setString(4, MatrixJson.encodeToString(JsonElement.serializer(), event.content))
+		eventStmt.setString(5, event.sender)
+		eventStmt.setString(6, event.unsigned?.let { MatrixJson.encodeToString(
+			UnsignedData.serializer(), it) })
+		eventStmt.setString(7, event.stateKey)
+		eventStmt.setString(8, event.prevContent?.let { MatrixJson.encodeToString(
+			JsonElement.serializer(), it) })
+		eventStmt.setLong(9, event.originServerTimestamp)
+		if (timelineOrder != null) {
+			eventStmt.setLong(10, timelineOrder)
+		} else {
+			eventStmt.setNull(10, Types.INTEGER)
+		}
+		return eventStmt.executeUpdate()
+	}
+
+	fun setPaginationToken(roomId: String, eventId: String, token: String): Int {
+		paginationTokenStmt.setString(1, roomId)
+		paginationTokenStmt.setString(2, eventId)
+		paginationTokenStmt.setString(3, token)
+		return paginationTokenStmt.executeUpdate()
+	}
+
+	fun updateAccountData(roomId: String?, type: String, content: JsonElement): Int {
+		accountStmt.setString(1, type)
+		accountStmt.setString(2, roomId)
+		accountStmt.setString(3, MatrixJson.encodeToString(JsonElement.serializer(), content))
+		return accountStmt.executeUpdate()
+	}
+
+	fun getLastOrder(roomId: String): Long {
+		lastOrderStmt.setString(1, roomId)
+		return lastOrderStmt.executeQuery().use { rs ->
+			check(rs.next())
+			rs.getLong(1)
+		}
+	}
+
+	fun createNewTimeline(roomId: String) {
+		newTimelineStmt.setString(1, roomId)
+		newTimelineStmt.executeUpdate()
+	}
+
+	fun insertDeviceEvent(type: String, sender: String, content: JsonElement): Int {
+		deviceEventStmt.setString(1, type)
+		deviceEventStmt.setString(2, MatrixJson.encodeToString(JsonElement.serializer(), content))
+		deviceEventStmt.setString(3, sender)
+		return deviceEventStmt.executeUpdate()
+	}
+
+	fun resetTrackedUser(userId: String): Int {
+		trackedUsersStmt.setString(1, userId)
+		return trackedUsersStmt.executeUpdate()
+	}
+
+	fun deleteTrackedUser(userId: String): Int {
+		removeTrackedUserStmt.setString(1, userId)
+		return removeTrackedUserStmt.executeUpdate()
+	}
+
+	override fun close() {
+		removeTrackedUserStmt.close()
+		trackedUsersStmt.close()
+		deviceEventStmt.close()
+		newTimelineStmt.close()
+		lastOrderStmt.close()
+		accountStmt.close()
+		paginationTokenStmt.close()
+		eventStmt.close()
+		roomSummaryStmt.close()
+	}
+}
+
+private fun storeSyncResponse(sync: SyncResponse, syncToken: String?) {
+	usingConnection { conn ->
+		conn.autoCommit = false
+
+		// Ensure sync token hasn't changed while we calling the endpoint.
+		// May happen if multiple instances of app are running.
+		if (syncToken != conn.getValue("SYNC_TOKEN")) {
+			println("Race condition when syncing")
+			return@usingConnection
+		}
+
+		conn.setValue("SYNC_TOKEN", sync.nextBatch)
+
+		InsertUtils(conn).use { utils ->
+			val rooms = sync.rooms
+			if (rooms != null) {
+				for ((roomId, joinedRoom) in rooms.join) {
+					val summary = joinedRoom.summary
+					if (summary != null) {
+						utils.updateRoomSummary(roomId, summary)
+					}
+
+					val timeline = joinedRoom.timeline
+					if (timeline != null) {
+						// Create new batch
+						var order = if (timeline.limited == true) {
+							utils.createNewTimeline(roomId)
+							1
+						} else {
+							utils.getLastOrder(roomId) + 1
+						}
+						for (event in timeline.events) {
+							utils.insertRoomEvent(roomId, event, order++)
+						}
+						if (timeline.limited == true) {
+							utils.setPaginationToken(roomId, timeline.events.first().eventId, timeline.prevBatch!!)
+						}
+					}
+
+					val stateEvents = joinedRoom.state?.events
+					if (stateEvents != null) {
+						for (event in stateEvents) {
+							utils.insertRoomEvent(roomId, event, null)
+						}
+					}
+
+					val accountEvents = joinedRoom.accountData?.events
+					if (accountEvents != null) {
+						for (event in accountEvents) {
+							utils.updateAccountData(roomId, event.type, event.content)
+						}
+					}
+				}
+			}
+
+			val accountData = sync.accountData
+			if (accountData != null) {
+				for (event in accountData.events) {
+					utils.updateAccountData(null, event.type, event.content)
+				}
+			}
+
+			val deviceEvents = sync.toDevice?.events
+			if (deviceEvents != null) {
+				for (event in deviceEvents) {
+					utils.insertDeviceEvent(event.type, event.sender!!, event.content)
+				}
+			}
+
+			val deviceLists = sync.deviceLists
+			if (deviceLists != null) {
+				for (userId in deviceLists.changed) {
+					utils.resetTrackedUser(userId)
+				}
+				// for (userId in deviceLists.left) {
+				// 	val updateCount = utils.deleteTrackedUser(userId)
+				// 	if (updateCount == 0) {
+				// 		println("User with id $userId left but we were not tracking them...")
+				// 	}
+				// }
+			}
+		}
+
+		conn.commit()
+	}
+}
--- src/main/kotlin/me/dominaezzz/chitchat/models/AppViewModel.kt
+++ src/main/kotlin/me/dominaezzz/chitchat/models/AppViewModel.kt
@@ -2,18 +2,12 @@ package me.dominaezzz.chitchat.models
 
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import io.github.matrixkt.MatrixClient
-import io.github.matrixkt.models.Presence
-import io.github.matrixkt.models.events.UnsignedData
-import io.github.matrixkt.models.sync.RoomSummary
 import io.github.matrixkt.models.sync.SyncResponse
-import io.github.matrixkt.utils.MatrixJson
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.sync.Semaphore
-import kotlinx.coroutines.sync.withPermit
 import kotlinx.serialization.json.*
 import me.dominaezzz.chitchat.db.*
-import java.sql.Types
 
 class AppViewModel(
 	private val client: MatrixClient,
@@ -22,185 +16,7 @@ class AppViewModel(
 	private val _syncFlow = MutableSharedFlow<SyncResponse>()
 
 	suspend fun sync() {
-		val syncToken = withContext(Dispatchers.IO) {
-			usingConnection { it.getValue("SYNC_TOKEN") }
-		}
-
-		println("Syncing with '$syncToken' as token")
-		val sync = client.eventApi.sync(since = syncToken, setPresence = Presence.OFFLINE, timeout = 100000)
-		println("Saving sync response")
-
-		dbSemaphore.withPermit {
-			withContext(Dispatchers.IO) {
-				usingConnection { conn ->
-					conn.autoCommit = false
-
-					// Ensure sync token hasn't changed while we calling the endpoint.
-					// May happen if multiple instances of app are running.
-					if (syncToken != conn.getValue("SYNC_TOKEN")) {
-						println("Race condition when syncing")
-						return@usingConnection
-					}
-
-					conn.setValue("SYNC_TOKEN", sync.nextBatch)
-
-					val rooms = sync.rooms
-					if (rooms != null) {
-						val joinedRooms = rooms.join
-						if (joinedRooms.isNotEmpty()) {
-							val roomStmt = conn.prepareStatement("""
-                                INSERT INTO room_metadata(roomId, summary)
-                                VALUES (?, ?)
-                                ON CONFLICT(roomId) DO UPDATE SET summary=JSON_PATCH(summary, excluded.summary);
-                            """)
-							val eventStmt = conn.prepareStatement("""
-                                INSERT INTO room_events(
-                                    roomId, eventId, type, content, sender, unsigned, stateKey, prevContent, timestamp,
-                                    timelineOrder
-                                )
-                                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-                                ON CONFLICT(roomId, eventId) DO UPDATE
-                                    SET timelineOrder = excluded.timelineOrder
-                                    WHERE excluded.timelineOrder IS NOT NULL;
-                            """)
-							val getLastOrderStmt = conn.prepareStatement("SELECT MAX(timelineOrder) FROM room_events WHERE roomId = ? AND timelineId = 0;")
-							val paginationTokenStmt = conn.prepareStatement("INSERT INTO room_pagination_tokens(roomId, eventId, token) VALUES (?, ?, ?);")
-							val newTimelineStmt = conn.prepareStatement("UPDATE room_events SET timelineId = timelineId + 1 WHERE roomId = ?;")
-							val accountStmt = conn.prepareStatement("INSERT OR REPLACE INTO account_data(type, roomId, content) VALUES (?, ?, ?);")
-							for ((roomId, joinedRoom) in joinedRooms) {
-								val summary = joinedRoom.summary
-								if (summary != null) {
-									roomStmt.setString(1, roomId)
-									roomStmt.setString(2, MatrixJson.encodeToString(RoomSummary.serializer(), summary))
-									roomStmt.executeUpdate()
-								}
-
-								val timeline = joinedRoom.timeline
-								if (timeline != null) {
-									// Create new batch
-									var order = if (timeline.limited == true) {
-										newTimelineStmt.setString(1, roomId)
-										newTimelineStmt.executeUpdate()
-										1
-									} else {
-										getLastOrderStmt.setString(1, roomId)
-										getLastOrderStmt.executeQuery().use {
-											check(it.next())
-											it.getLong(1) + 1
-										}
-									}
-									for (event in timeline.events) {
-										eventStmt.setString(1, roomId)
-										eventStmt.setString(2, event.eventId)
-										eventStmt.setString(3, event.type)
-										eventStmt.setString(4, MatrixJson.encodeToString(JsonElement.serializer(), event.content))
-										eventStmt.setString(5, event.sender)
-										eventStmt.setString(6, event.unsigned?.let { MatrixJson.encodeToString(
-											UnsignedData.serializer(), it) })
-										eventStmt.setString(7, event.stateKey)
-										eventStmt.setString(8, event.prevContent?.let { MatrixJson.encodeToString(
-											JsonElement.serializer(), it) })
-										eventStmt.setLong(9, event.originServerTimestamp)
-										eventStmt.setLong(10, order++)
-										eventStmt.executeUpdate()
-									}
-									if (timeline.limited == true) {
-										paginationTokenStmt.setString(1, roomId)
-										paginationTokenStmt.setString(2, timeline.events.first().eventId)
-										paginationTokenStmt.setString(3, timeline.prevBatch!!)
-										paginationTokenStmt.executeUpdate()
-									}
-								}
-
-								val stateEvents = joinedRoom.state?.events
-								if (stateEvents != null) {
-									for (event in stateEvents) {
-										eventStmt.setString(1, roomId)
-										eventStmt.setString(2, event.eventId)
-										eventStmt.setString(3, event.type)
-										eventStmt.setString(4, MatrixJson.encodeToString(JsonElement.serializer(), event.content))
-										eventStmt.setString(5, event.sender)
-										eventStmt.setString(6, event.unsigned?.let { MatrixJson.encodeToString(
-											UnsignedData.serializer(), it) })
-										eventStmt.setString(7, event.stateKey)
-										eventStmt.setString(8, event.prevContent?.let { MatrixJson.encodeToString(
-											JsonElement.serializer(), it) })
-										eventStmt.setLong(9, event.originServerTimestamp)
-										eventStmt.setNull(10, Types.INTEGER)
-										eventStmt.executeUpdate()
-									}
-								}
-
-								val accountEvents = joinedRoom.accountData?.events
-								if (accountEvents != null) {
-									for (event in accountEvents) {
-										accountStmt.setString(1, event.type)
-										accountStmt.setString(2, roomId)
-										accountStmt.setString(3, MatrixJson.encodeToString(JsonElement.serializer(), event.content))
-										accountStmt.executeUpdate()
-									}
-								}
-							}
-							accountStmt.close()
-							newTimelineStmt.close()
-							paginationTokenStmt.close()
-							getLastOrderStmt.close()
-							eventStmt.close()
-							roomStmt.close()
-						}
-					}
-
-					val accountData = sync.accountData
-					if (accountData != null && accountData.events.isNotEmpty()) {
-						conn.prepareStatement(
-							"""
-                            INSERT OR REPLACE INTO account_data(type, roomId, content)
-                            VALUES (?, NULL, ?);
-                            """).use { stmt ->
-							for (event in accountData.events) {
-								stmt.setString(1, event.type)
-								stmt.setString(2, MatrixJson.encodeToString(JsonElement.serializer(), event.content))
-								stmt.executeUpdate()
-							}
-						}
-					}
-
-					val deviceEvents = sync.toDevice?.events
-					if (deviceEvents != null) {
-						conn.prepareStatement("INSERT INTO device_events(type, content, sender) VALUES (?, ?, ?);").use { stmt ->
-							for (event in deviceEvents) {
-								stmt.setString(1, event.type)
-								stmt.setString(2, MatrixJson.encodeToString(JsonElement.serializer(), event.content))
-								stmt.setString(3, event.sender)
-								stmt.executeUpdate()
-							}
-						}
-					}
-
-					val deviceLists = sync.deviceLists
-					if (deviceLists != null) {
-						conn.prepareStatement("UPDATE tracked_users SET deviceListState = 0 WHERE userId = ?;").use { stmt ->
-							for (userId in deviceLists.changed) {
-								stmt.setString(1, userId)
-								stmt.executeUpdate()
-							}
-						}
-						// conn.prepareStatement("DELETE FROM tracked_users WHERE userId = ?;").use { stmt ->
-						//     for (userId in deviceLists.left) {
-						//         stmt.setString(1, userId)
-						//         val updateCount = stmt.executeUpdate()
-						//         if (updateCount == 0) {
-						//             println("User with id $userId left but we were not tracking them...")
-						//         }
-						//     }
-						// }
-					}
-
-					conn.commit()
-				}
-			}
-		}
-
+		val sync = sync(client, dbSemaphore)
 		_syncFlow.emit(sync)
 	}
 
