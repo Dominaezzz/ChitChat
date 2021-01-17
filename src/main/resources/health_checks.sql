0731cf0a67caa51c6fc2e4ca0c5560eef4bf5985
Dominic Fischer <dominicfischer7@gmail.com>
2021-01-17 20:48:35 +0000

    Add health check

--- src/main/resources/health_checks.sql
+++ src/main/resources/health_checks.sql
@@ -1,12 +1,3 @@
--- Check that all timeline state events have prevContents
-SELECT roomId, type, stateKey, eventId, rowNum
-FROM (
-    SELECT ROW_NUMBER() OVER (PARTITION BY roomId, type, stateKey ORDER BY timelineId DESC, timelineOrder ASC) AS rowNum, *
-    FROM room_events
-    WHERE stateKey IS NOT NULL
-)
-WHERE timelineOrder IS NOT NULL AND prevContent IS NULL AND rowNum != 1;
-
 -- Check that timelineOrder is contiguous
 SELECT roomId, eventId, timelineId, timelineOrder, rowNum
 FROM (
@@ -15,3 +6,16 @@ FROM (
     WHERE timelineOrder IS NOT NULL
 )
 WHERE timelineOrder != rowNum;
+
+-- Check that the latest state is marked properly.
+WITH
+     event_latest(roomId, eventId, latestEventId) AS (
+         SELECT roomId, eventId, FIRST_VALUE(eventId) OVER (PARTITION BY roomId, type, stateKey ORDER BY timelineId ASC, timelineOrder DESC)
+         FROM room_events
+         WHERE stateKey IS NOT NULL
+     )
+SELECT *
+FROM room_events
+JOIN event_latest USING (roomId, eventId)
+WHERE isLatestState != (eventId == latestEventId)
+ORDER BY roomId, type, stateKey, timelineId ASC, timelineOrder DESC;
