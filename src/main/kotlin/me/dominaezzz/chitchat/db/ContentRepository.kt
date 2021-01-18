5ab64cbd912675242e873126ac2bd9903eb8a9bc
Dominic Fischer <dominicfischer7@gmail.com>
2021-01-18 19:48:58 +0000

    Formatting fail

--- src/main/kotlin/me/dominaezzz/chitchat/db/ContentRepository.kt
+++ src/main/kotlin/me/dominaezzz/chitchat/db/ContentRepository.kt
@@ -9,40 +9,40 @@ import java.nio.file.Files
 import java.nio.file.Path
 
 class ContentRepository(private val client: MatrixClient, private val mediaDir: Path) {
-    private val scope = CoroutineScope(SupervisorJob())
-    private val serverSemaphore = Semaphore(2)
-    private val ioSemaphore = Semaphore(5)
+	private val scope = CoroutineScope(SupervisorJob())
+	private val serverSemaphore = Semaphore(2)
+	private val ioSemaphore = Semaphore(5)
 
-    suspend fun getContent(uri: URI): ByteArray {
-        require(uri.scheme == "mxc")
+	suspend fun getContent(uri: URI): ByteArray {
+		require(uri.scheme == "mxc")
 
-        val serverName = uri.host
-        val mediaId = uri.path.removePrefix("/")
+		val serverName = uri.host
+		val mediaId = uri.path.removePrefix("/")
 
-        val mediaFile = mediaDir.resolve(serverName).resolve(mediaId)
-        return if (Files.exists(mediaFile)) {
-            ioSemaphore.withPermit {
-                withContext(Dispatchers.IO) {
-                    Files.readAllBytes(mediaFile)
-                }
-            }
-        } else {
-            val data = serverSemaphore.withPermit {
-                client.contentApi.getContent(serverName, mediaId, true)
-            }
-            scope.launch(Dispatchers.Default) {
-                ioSemaphore.withPermit {
-                    withContext(Dispatchers.IO) {
-                        Files.createDirectories(mediaFile.parent)
-                        Files.write(mediaFile, data)
-                    }
-                }
-            }
-            data
-        }
-    }
+		val mediaFile = mediaDir.resolve(serverName).resolve(mediaId)
+		return if (Files.exists(mediaFile)) {
+			ioSemaphore.withPermit {
+				withContext(Dispatchers.IO) {
+					Files.readAllBytes(mediaFile)
+				}
+			}
+		} else {
+			val data = serverSemaphore.withPermit {
+				client.contentApi.getContent(serverName, mediaId, true)
+			}
+			scope.launch(Dispatchers.Default) {
+				ioSemaphore.withPermit {
+					withContext(Dispatchers.IO) {
+						Files.createDirectories(mediaFile.parent)
+						Files.write(mediaFile, data)
+					}
+				}
+			}
+			data
+		}
+	}
 
-    fun close() {
-        scope.cancel()
-    }
+	fun close() {
+		scope.cancel()
+	}
 }
--- src/main/kotlin/me/dominaezzz/chitchat/db/SQLUtils.kt
+++ src/main/kotlin/me/dominaezzz/chitchat/db/SQLUtils.kt
@@ -12,107 +12,107 @@ import kotlin.contracts.InvocationKind
 import kotlin.contracts.contract
 
 fun getConnection(dbFile: Path): Connection {
-    val config = SQLiteConfig()
-    config.enforceForeignKeys(true)
-    config.setJournalMode(SQLiteConfig.JournalMode.WAL)
-    // config.busyTimeout = 5000
+	val config = SQLiteConfig()
+	config.enforceForeignKeys(true)
+	config.setJournalMode(SQLiteConfig.JournalMode.WAL)
+	// config.busyTimeout = 5000
 
-    return DriverManager.getConnection("jdbc:sqlite:${dbFile.toAbsolutePath()}", config.toProperties())
+	return DriverManager.getConnection("jdbc:sqlite:${dbFile.toAbsolutePath()}", config.toProperties())
 }
 
 @OptIn(ExperimentalContracts::class)
 inline fun <T> usingConnection(block: (Connection) -> T): T {
-    contract {
-        callsInPlace(block, InvocationKind.EXACTLY_ONCE)
-    }
-    return getConnection(projectDir.resolve("app.db")).use(block)
+	contract {
+		callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+	}
+	return getConnection(projectDir.resolve("app.db")).use(block)
 }
 
 @OptIn(ExperimentalContracts::class)
 inline fun <T> Connection.usingStatement(block: (Statement) -> T): T {
-    contract {
-        callsInPlace(block, InvocationKind.EXACTLY_ONCE)
-    }
-    return createStatement().use(block)
+	contract {
+		callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+	}
+	return createStatement().use(block)
 }
 
 @OptIn(ExperimentalContracts::class)
 inline fun <T> Connection.savepoint(block: () -> T): T {
-    contract {
-        callsInPlace(block, InvocationKind.EXACTLY_ONCE)
-    }
+	contract {
+		callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+	}
 
-    val savepoint = setSavepoint()
+	val savepoint = setSavepoint()
 
-    var exception: Throwable? = null
-    try {
-        return block()
-    } catch (e: Throwable) {
-        exception = e
-        throw e
-    } finally {
-        if (exception != null) {
-            try {
-                rollback(savepoint)
-            } catch (rollbackException: Throwable) {
-                @Suppress("UNNECESSARY_SAFE_CALL") // https://youtrack.jetbrains.com/issue/KT-28806
-                exception?.addSuppressed(rollbackException)
-            }
-        } else {
-            releaseSavepoint(savepoint)
-        }
-    }
+	var exception: Throwable? = null
+	try {
+		return block()
+	} catch (e: Throwable) {
+		exception = e
+		throw e
+	} finally {
+		if (exception != null) {
+			try {
+				rollback(savepoint)
+			} catch (rollbackException: Throwable) {
+				@Suppress("UNNECESSARY_SAFE_CALL") // https://youtrack.jetbrains.com/issue/KT-28806
+				exception?.addSuppressed(rollbackException)
+			}
+		} else {
+			releaseSavepoint(savepoint)
+		}
+	}
 }
 
 @OptIn(ExperimentalContracts::class)
 inline fun <T> Connection.withoutIndex(tableName: String, indexName: String, block: () -> T) {
-    contract {
-        callsInPlace(block, InvocationKind.EXACTLY_ONCE)
-    }
+	contract {
+		callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+	}
 
-    savepoint {
-        val ddlStmt = prepareStatement("SELECT sql FROM sqlite_master WHERE type = ? AND tbl_name = ? AND name = ?").use { stmt ->
-            stmt.setString(1, "index")
-            stmt.setString(2, tableName)
-            stmt.setString(3, indexName)
-            stmt.executeQuery().use { rs ->
-                if (!rs.next()) {
-                    throw NoSuchElementException("Index($indexName) on Table '${tableName}' does not exist!")
-                }
-                rs.getString(1)
-            }
-        }
-        usingStatement { stmt ->
-            stmt.execute("DROP INDEX $indexName;") // No way around this concatenation sadly.
-            block()
-            stmt.execute(ddlStmt)
-        }
-    }
+	savepoint {
+		val ddlStmt = prepareStatement("SELECT sql FROM sqlite_master WHERE type = ? AND tbl_name = ? AND name = ?").use { stmt ->
+			stmt.setString(1, "index")
+			stmt.setString(2, tableName)
+			stmt.setString(3, indexName)
+			stmt.executeQuery().use { rs ->
+				if (!rs.next()) {
+					throw NoSuchElementException("Index($indexName) on Table '${tableName}' does not exist!")
+				}
+				rs.getString(1)
+			}
+		}
+		usingStatement { stmt ->
+			stmt.execute("DROP INDEX $indexName;") // No way around this concatenation sadly.
+			block()
+			stmt.execute(ddlStmt)
+		}
+	}
 }
 
 fun <T> ResultSet.getSerializable(columnLabel: String, deserializer: DeserializationStrategy<T>): T? {
-    val content = getString(columnLabel)
-    return if (content != null) {
-        MatrixJson.decodeFromString(deserializer, content)
-    } else {
-        null
-    }
+	val content = getString(columnLabel)
+	return if (content != null) {
+		MatrixJson.decodeFromString(deserializer, content)
+	} else {
+		null
+	}
 }
 
 fun <T> ResultSet.getSerializable(columnIndex: Int, deserializer: DeserializationStrategy<T>): T? {
-    val content = getString(columnIndex)
-    return if (content != null) {
-        MatrixJson.decodeFromString(deserializer, content)
-    } else {
-        null
-    }
+	val content = getString(columnIndex)
+	return if (content != null) {
+		MatrixJson.decodeFromString(deserializer, content)
+	} else {
+		null
+	}
 }
 
 fun <T> PreparedStatement.setSerializable(parameterIndex: Int, serializer: SerializationStrategy<T>, value: T?) {
-    if (value != null) {
-        val content = MatrixJson.encodeToString(serializer, value)
-        setString(parameterIndex, content)
-    } else {
-        setString(parameterIndex, null)
-    }
+	if (value != null) {
+		val content = MatrixJson.encodeToString(serializer, value)
+		setString(parameterIndex, content)
+	} else {
+		setString(parameterIndex, null)
+	}
 }
